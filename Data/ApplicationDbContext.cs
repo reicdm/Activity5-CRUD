8c95b747fc909384f5673cafc20915df3d03e531
reicdm <reicdmagpantay@gmail.com>
2026-05-13 00:46:01 +0800

    initial setup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Activity5_CRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+        }
+    }
+}
---
+++ Models/Entities/Art.cs
@@ -0,0 +1,6 @@
+﻿namespace Activity5_CRUD.Models.Entities
+{
+    public class Art
+    {
+    }
+}
