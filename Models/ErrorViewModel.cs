de67e9af5f9c13973ed48a74c2027f65a3c634be
reicdm <reicdmagpantay@gmail.com>
2026-05-13 00:23:54 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Activity5_CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
